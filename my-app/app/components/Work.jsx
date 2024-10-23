2dd3fcaa7af61d5aefcce35cb3230710cf352dde
Shivani <shivanitripurari07@gmail.com>
2024-10-23 22:33:25 +0530

    update

--- my-app/app/components/Work.jsx
+++ my-app/app/components/Work.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Image from 'next/image'
-
+import Link from 'next/link';
 import star2 from '../assets/star2.svg';
 import redirect from '../assets/redirectW.svg';
 
-const Work = ({ workText = "",full="", items = [] }) => {
+const Work = ({ workText = "",full="",linkUrl="", items = [] }) => {
   return (
     <>
     <div className="flex justify-center items-center relative top-10 left-0">
@@ -12,11 +12,11 @@ const Work = ({ workText = "",full="", items = [] }) => {
         <div className="absolute h-full w-full">
           <Image src={star2} alt='star' className="hidden h-4 w-4 absolute top-16 left-[20] md:left-[30rem] lg:left-[30rem] group-hover:block"/>
           <Image src={star2} alt='star' className="hidden h-8 w-8 absolute bottom-16 left-40 group-hover:block" />
-          <Image src={redirect} alt='redirect' className="hidden h-6 w-6 absolute top-4 right-4 md:right-16 lg:right-20 group-hover:block"/>
+          <Link href={linkUrl}><Image src={redirect} alt='redirect' className="hidden h-6 w-6 absolute top-4 right-4 md:right-16 lg:right-20 group-hover:block"/></Link>
         </div>
         <div className="text-transparent transition duration-500 ease-in-out font-outline-2 font-semibold text-[8rem] md:text-[8rem] lg:text-[16rem] mx-7 group-hover:scale-[1.7] group-hover:font-bold group-hover:-translate-x-8 md:group-hover:-translate-x-28 lg:group-hover:-translate-x-28 group-hover:font-outline-1">{workText}</div>
         <div className="flex flex-col justify-between  items-left">
-          <h1 class="transition duration-500 ease-in-out text-2xl md:text-2xl lg:text-4xl text-[#fefefe] font-bold group-hover:scale-75  group-hover:font-outline-2 group-hover:text-transparent">{full}</h1>
+          <h1 className="transition duration-500 ease-in-out text-2xl md:text-2xl lg:text-4xl text-[#fefefe] font-bold group-hover:scale-75  group-hover:font-outline-2 group-hover:text-transparent">{full}</h1>
           <ul className="hidden md:hidden lg:block transition duration-500 ease-in-out text-base md:text-lg lg:text-lg text-slate-200 ml-7 pt-7 group-hover:scale-75 group-hover:text-slate-300">
             {items.map((item,index)=>(
                 <p className="w-3/4" key={index}>{item}</p>
